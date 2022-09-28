e8a4fb3a08e847f638a42b2839ac5d0db49dfe1f
Justin Wade <jpwade109@gmail.com>
2022-09-28 19:33:26 -0400

    started inventory methods

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/InventoryController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/InventoryController.java
@@ -1,5 +1,14 @@
 package com.estore.api.estoreapi.controller;
+import com.estore.api.estoreapi.model.Item;
 
 public class InventoryController {
+    private Item item;
+
+
+    public Item search() {
+        return this.item;
+    } 
+    
+
     
 }
