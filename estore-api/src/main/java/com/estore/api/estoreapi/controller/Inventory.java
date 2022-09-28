942af11a5a7c317cbb75b390b5579fdf3b4e0efd
Justin Talbot <jmt8032@g.rit.edu>
2022-09-28 19:19:23 -0400

    Decided on design for inventory

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/Inventory.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/Inventory.java
@@ -1,5 +1,17 @@
 package com.estore.api.estoreapi.controller;
 
+import java.util.HashMap;
+
 public class Inventory {
-    
+    /** A hashmap representation of the inventory where the key is the name
+     * of the item and the value is the item object */
+    HashMap<String, Item> inventory;
+
+    /**
+     * 
+     * @param inventory
+     */
+    public Inventory(HashMap<String, Item> inventory) {
+        this.inventory = inventory;
+    }
 }
