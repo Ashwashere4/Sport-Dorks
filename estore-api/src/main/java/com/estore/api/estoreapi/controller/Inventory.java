f546db5193fe07883bfe39c922d0e6ee4e288ee4
Cadenmf24 <Cadenmf24@gmail.com>
2022-09-28 19:21:01 -0400

    yup, thats spicy

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/Inventory.java
@@ -0,0 +1,10 @@
+package com.estore.api.estoreapi.controller;
+
+public class Inventory {
+    
+    public static void new_product(String product_name, int product_price, int product_stock_amount){
+
+        
+    }
+    
+}
