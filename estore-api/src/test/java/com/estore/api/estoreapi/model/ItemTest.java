d14b2051643bde5f21011302e89281683dcc986a
Cadenmf24 <Cadenmf24@gmail.com>
2022-10-16 21:10:16 -0400

    itemtest

---
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/ItemTest.java
@@ -0,0 +1,27 @@
+package com.estore.api.estoreapi.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("Model-tier")
+public class ItemTest {
+
+
+    @Test
+    public void testCtor(){
+
+        int expected_cost = 12;
+        int expected_quantity = 10;
+        String expected_name = "This wild Drip";
+
+        Item item = new Item(expected_name,expected_cost,expected_quantity);
+
+        assertEquals(expected_name, item.getName());
+        assertEquals(expected_cost, item.getCost());
+        assertEquals(expected_quantity, item.getQuantity());
+    }
+
+    
+}
