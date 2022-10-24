e9ca5536777a034bd4d9e04130559c7380ab3474
Justin Wade <jpwade109@gmail.com>
2022-10-23 23:49:24 -0400

    nav bar

--- estore-ui/src/app/app.module.ts
+++ estore-ui/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
