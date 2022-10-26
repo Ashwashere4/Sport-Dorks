e1ba1eaac695bf9e54cdf41ef1f9396ef324f80a
kalen-luraschi <kalenluraschi@gmail.com>
2022-10-26 02:11:35 -0400

    wishlist commands running

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/wishlist/WishListController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/wishlist/WishListController.java
@@ -2,7 +2,11 @@ package com.estore.api.estoreapi.controller.wishlist;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +26,7 @@ public class WishListController {
         this.wishListDAO = wishListDAO;
     }
 
-    @GetMapping()
+    @GetMapping("")
     public ResponseEntity<Item[]> getItems() {
         LOG.info("GET /wishList");
         Item[] items;
@@ -31,6 +35,49 @@ public class WishListController {
             return new ResponseEntity<Item[]>(items, HttpStatus.OK);
         } catch (IOException e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/{name}")
+    public ResponseEntity<Item> addItem(@PathVariable String name){
+        LOG.info("POST /wishList/" + name);
+        try{
+            Item newItem = wishListDAO.addItem(name);
+            return new ResponseEntity<Item>(newItem,HttpStatus.CREATED);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<Item>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{name}")
+    public ResponseEntity<Boolean> deleteItem(@PathVariable String name) {
+        LOG.info("DELETE /wishList/" + name);
+        try {
+            this.wishListDAO.deleteItem(name);
+            if (this.wishListDAO.deleteItem(name) == false){
+                return new ResponseEntity<Boolean>(false, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<Boolean>(true,HttpStatus.OK);
+        } catch (IOException e) {
+            System.out.println("Item not found.");
+            return new ResponseEntity<Boolean>(false,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{name}")
+    public ResponseEntity<Boolean> addItemToCart(@PathVariable String name) {
+        LOG.info("PUT /ShoppingCart/" + name);
+        try {
+            this.wishListDAO.addItemToCart(name);
+            if (this.wishListDAO.deleteItem(name) == false){
+                return new ResponseEntity<Boolean>(false, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<Boolean>(true,HttpStatus.OK);
+        } catch (IOException e) {
+            System.out.println("Item not found.");
+            return new ResponseEntity<Boolean>(false,HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
