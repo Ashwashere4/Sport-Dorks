ac3e7befe4e6ad241857143b0028f621bf2e63d5
kalen-luraschi <kalenluraschi@gmail.com>
2022-10-26 01:51:34 -0400

    fully working shopping cart with commands usable

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/shoppingCart/ShoppingCartController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/shoppingCart/ShoppingCartController.java
@@ -2,7 +2,11 @@ package com.estore.api.estoreapi.controller.shoppingCart;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +26,7 @@ public class ShoppingCartController {
         this.shoppingCartDAO = shoppingCartDAO;
     }
 
-    @GetMapping()
+    @GetMapping("")
     public ResponseEntity<Item[]> getCart() {
         LOG.info("GET /shoppingCart");
         Item[] items;
@@ -31,6 +35,49 @@ public class ShoppingCartController {
             return new ResponseEntity<Item[]>(items, HttpStatus.OK);
         } catch (IOException e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/{name}")
+    public ResponseEntity<Item> addItem(@PathVariable String name){
+        LOG.info("POST /shoppingCart/" + name);
+        try{
+            Item newItem = shoppingCartDAO.addItem(name);
+            return new ResponseEntity<Item>(newItem,HttpStatus.CREATED);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<Item>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{name}")
+    public ResponseEntity<Boolean> deleteItem(@PathVariable String name) {
+        LOG.info("DELETE /ShoppingCart/" + name);
+        try {
+            this.shoppingCartDAO.deleteItem(name);
+            if (this.shoppingCartDAO.deleteItem(name) == false){
+                return new ResponseEntity<Boolean>(false, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<Boolean>(true,HttpStatus.OK);
+        } catch (IOException e) {
+            System.out.println("Item not found.");
+            return new ResponseEntity<Boolean>(false,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{name}")
+    public ResponseEntity<Boolean> purchaseItem(@PathVariable String name) {
+        LOG.info("PUT /ShoppingCart/" + name);
+        try {
+            this.shoppingCartDAO.purchaseItem(name);
+            if (this.shoppingCartDAO.deleteItem(name) == false){
+                return new ResponseEntity<Boolean>(false, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<Boolean>(true,HttpStatus.OK);
+        } catch (IOException e) {
+            System.out.println("Item not found.");
+            return new ResponseEntity<Boolean>(false,HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
