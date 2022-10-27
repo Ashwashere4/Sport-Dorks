c5053dc48af681dae9467d85e60bb8af9a4b2235
Cadenmf24 <Cadenmf24@gmail.com>
2022-10-26 22:40:50 -0400

    added facilities page

---
+++ estore-ui/src/app/facilities/facilities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './facilities.component.html',
+  styleUrls: ['./facilities.component.css']
+})
+export class facilitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
