569218cfb9b3ce5b3222949e74beaf23229ba973
Cadenmf24 <Cadenmf24@gmail.com>
2022-11-14 18:45:02 -0500

    finish this later

--- estore-api/src/main/java/com/estore/api/estoreapi/model/facilities/Facilities.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/facilities/Facilities.java
@@ -5,14 +5,14 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+
 
 
 public class Facilities {
     @JsonProperty("name") private String name;
     @JsonProperty("location") private String location;
     @JsonProperty("facility_id") private int facility_id;
-    @JsonProperty("team_reserve") private Team team_reserve;
+    @JsonProperty("team_reserve") private Boolean team_reserve;
 
     /**
      * Constructor for an item object
@@ -25,7 +25,7 @@ public class Facilities {
         this.name = name;
         this.location = location;
         this.facility_id = facility_id;
-        this.team_reserve = null;
+        this.team_reserve = false;
     };
 
     /**
@@ -53,16 +53,16 @@ public class Facilities {
     }
 
     public Boolean getReservestatus(){
-        return this.team_reserve != null;
+        return this.team_reserve != false;
     }
 
-    public Team getTeam_reserve(){
+    public Boolean getTeam_reserve(){
 
         return this.team_reserve;
     }
 
     public Boolean removeTeam_reserve(){
-        this.team_reserve = null;
+        this.team_reserve = false;
 
         return true;
     }
@@ -71,7 +71,6 @@ public class Facilities {
 
         if (getReservestatus() == false){
 
-            this.team_reserve = team1;
 
             return true;
         }
@@ -96,7 +95,7 @@ public class Facilities {
      * Method to get the string representation of an item object
      */
     public String toString(){
-        return ("Facility Name = " + name + "\nFacility Location= " + location + "\nFacility Id = " + facility_id + "Reserved by: " + getTeam_reserve());
+        return ("Facility Name = " + name + "\nFacility Location= " + location + "\nFacility Id = " + facility_id + "Reserved?: " + getTeam_reserve());
     }
 }
 
